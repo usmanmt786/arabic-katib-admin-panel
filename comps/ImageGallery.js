4e847d610f821cf96b28c533aaebc4aebd1ce1c6
usmanmt786 <88397820+usmanmt786@users.noreply.github.com>
2023-06-25 13:31:20 +0530

    Update ImageGallery.js

--- comps/ImageGallery.js
+++ comps/ImageGallery.js
@@ -34,7 +34,7 @@ getImages()
 
 return(
 <div style={{background:"rgb(0, 0, 0,.5)"}} className="overflow-auto w-full h-screen fixed top-0 left-0 right-0  flex items-center z-10 justify-center">
-<div className="w-full mx-2 md:w-3/5 bg-zinc-100 rounded  h-4/6 flex flex-col">
+<div className="w-11/12  md:w-3/5 bg-zinc-100 rounded  h-4/6 flex flex-col">
 <div className="rounded-t p-1 bg-zinc-200 flex justify-between">
 <h4 className="text-xl text-zinc-500 rubik font-semibold">Image Gallery</h4>
 <AiFillCloseCircle className="text-2xl text-zinc-500 hover:text-zinc-600 cursor-pointer active:text-zinc-600" onClick={()=>{
@@ -42,7 +42,7 @@ setGallery(false)
 }}/>
 </div>
 <div className="flex flex-col md:flex-row py-1 gap-1 h-full  overflow-hidden">
-<div className={`w-3/4 bg-zinc-200 rounded-b overflow-auto  p-2 h-full ${loading ? 'flex text-lg rubik justify-center items-center' : 'grid grid-cols-1 md:grid-cols-4 gap-2'}`}>
+<div className={`md:w-3/4 w-full bg-zinc-200 rounded-b overflow-auto  p-2 h-full ${loading ? 'flex text-lg rubik justify-center items-center' : 'grid grid-cols-1 md:grid-cols-4 gap-2'}`}>
 {loading ? 'Loading...' : images.map((image,index)=>(
 <div className={`post-card ${selected.name===image.name && 'relative p-1 bg-blue-400 rounded'}`} onClick={()=>{
 setSelected(image)
@@ -52,7 +52,7 @@ setSelected(image)
 </div>
 ))}
 </div>
-<div className="w-1/4 bg-zinc-200 rounded-b h-full flex flex-col justify-between items-center p-2">
+<div className="md:w-1/4 w-full bg-zinc-200 rounded-b h-full flex md:flex-col justify-between items-center p-2">
 {Object.keys(selected).length === 0 ? <h3 className="text-lg text-center py-5 rubik text-zinc-400">Select Image
 </h3>:<div className="rubik font-semibold flex flex-col gap-2">
 <div className="flex gap-1">
