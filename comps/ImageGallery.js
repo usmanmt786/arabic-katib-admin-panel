a5c2e95afe6076c1711e36cdec97515a8615349e
usmanmt786 <88397820+usmanmt786@users.noreply.github.com>
2023-06-25 13:42:30 +0530

    Update ImageGallery.js

--- comps/ImageGallery.js
+++ comps/ImageGallery.js
@@ -38,7 +38,7 @@ return(
 <div className="rounded-t p-1 bg-zinc-200 flex justify-between">
 <h4 className="text-xl text-zinc-500 rubik font-semibold">Image Gallery</h4>
 <AiFillCloseCircle className="text-2xl text-zinc-500 hover:text-zinc-600 cursor-pointer active:text-zinc-600" onClick={()=>{
-document.body.style.overflow="auto"
+document.body.style.overflow="scroll"
 setGallery(false)
 }}/>
 </div>
@@ -56,11 +56,11 @@ setSelected(image)
 <div className="md:w-1/4 w-full bg-zinc-200 rounded-b h-full flex md:flex-col justify-between items-center p-2">
 {Object.keys(selected).length === 0 ? <h3 className="text-lg text-center py-5 rubik text-zinc-400">Select Image
 </h3>:<div className="rubik font-semibold flex flex-col gap-2">
-<div className="flex gap-1">
+<div className="flex flex-col md:flex-row gap-1">
 <h4>Name:</h4>
 <h4 className="whitespace-normal break-all">{selected.name}</h4>
 </div>
-<div className="flex gap-1">
+<div className="flex gap-1 flex-col md:flex-row">
 <h4>Size:</h4>
 <h4>{Math.round(selected.size/1024)} kbs</h4>
 </div>
