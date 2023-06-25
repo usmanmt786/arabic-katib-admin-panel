abb57a99e6b01cf134266995f7f14a7a52bd4d2f
usmanmt786 <88397820+usmanmt786@users.noreply.github.com>
2023-06-25 14:46:29 +0530

    Update ImageGallery.js

--- comps/ImageGallery.js
+++ comps/ImageGallery.js
@@ -1,4 +1,4 @@
-import React, {useState,useEffect} from 'react'
+⁰import React, {useState,useEffect} from 'react'
 import {AiFillCloseCircle} from 'react-icons/ai'
 import getUploads from '@/utils/getUploads'
 import {TiTick} from 'react-icons/ti'
@@ -53,7 +53,7 @@ setSelected(image)
 </div>
 ))}
 </div>
-<div className="md:w-1/4 w-full bg-zinc-200 rounded-b h-full flex flex-col justify-between items-center p-2">
+<div className="md:w-1/4 w-full bg-zinc-200 rounded-b md:h-full flex flex-col justify-between items-center p-2">
 {Object.keys(selected).length === 0 ? <h3 className="text-lg text-center py-5 rubik text-zinc-400">Select Image
 </h3>:<div className="rubik font-semibold flex flex-col gap-2">
 <div className="flex gap-1">
