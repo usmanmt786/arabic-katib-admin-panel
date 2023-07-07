6945234aa1f07333f0865f2c68be078fa58954e1
usmanmt786 <88397820+usmanmt786@users.noreply.github.com>
2023-07-07 21:18:57 +0530

    Update AddPostForm.js

--- comps/AddPostForm.js
+++ comps/AddPostForm.js
@@ -134,7 +134,7 @@ value={title}
 
 <MultipleSelect label='Next posts' content={posts} value={nexts} action={setNexts} id="nexts"  setValidation={setValidation} type="post"/>
 
-<ValidationButton state={state} validate={postFormValidate} add={addPost}    formData={formData} setErrors={setErrors} setState={setState} setValidation={setValidation} validation={validation} redirect="posts"/>
+<ValidationButton state={state} validate={postFormValidate} add={addPost}    formData={formData} setErrors={setErrors} setState={setState} setValidation={setValidation} validation={validation} redirect="posts" post="true"/>
           
             
         </div>
